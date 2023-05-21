4dc2cf2d8c63095baca5333efa74ac037ec70e43
Liam Maeder <liam.maeder@gmail.com>
2023-05-21 22:20:29 +0200

    fixed file structure issues

---
+++ main.js
@@ -0,0 +1,59 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "01/01/2023", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }.bind(this);
+
+        return true;
+    }
+}
+
+let laimCoin = new Blockchain();
+laimCoin.addBlock(new Block(1, "12/02/2023", {amount: 2}));
+laimCoin.addBlock(new Block(2, "24/03/2023", {amount: 3.5}));
+
+// console.log(JSON.stringify(laimCoin, null, 4));
+console.log("IS blockchain valid? " + laimCoin.isChainValid());
